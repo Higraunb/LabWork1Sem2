15c9b090fac965e1fc53301fb9a76cdd71fb54c3
higraunb <bear.porf@gmail.com>
2025-03-14 08:23:08 +0300

    new commit

--- test/test_Ground.cpp
+++ test/test_Ground.cpp
@@ -12,11 +12,3 @@ TEST(TGround, can_get_speed)
   TGround bf;
   EXPECT_DOUBLE_EQ(bf.GetSpeed(), 0.0);
 }
-///ДОЛБАЕБ
-/*
-TEST(Ground, can_delete_Ground)
-{
-  Ground bf;
-  ASSERT_NO_THROW(bf.~Ground());
-}
-*/
